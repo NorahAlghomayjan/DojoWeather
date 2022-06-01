28e3c391e3da855c3d03acd85569e9537fdcf36f
Norah Alghomayjan <noor.workspace@gmail.com>
2022-06-01 03:48:17 +0300

    commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var tempreture = document.querySelector("#tempereture");
+var heighests = document.querySelectorAll(".highest");
+var lowests = document.querySelectorAll(".lowest");
+
+tempreture.addEventListener("change",changeTemp);
+
+function changeTemp ()
+{
+    switch (tempreture.value) 
+    {
+        case "F" :
+            for (i=0;i<heighests.length; i++)
+            { 
+                heighests[i].innerText = Math.round(parseInt(heighests[i].innerText) * 1.8 + 32);
+                lowests[i].innerText = Math.round(parseInt(lowests[i].innerText) * 1.8 + 32);
+            };
+            break;
+        case "C" :
+            for (i=0;i<heighests.length; i++)
+            { 
+                heighests[i].innerText = Math.round((parseInt(heighests[i].innerText) - 32) * 0.5556);
+                lowests[i].innerText = Math.round((parseInt(lowests[i].innerText) - 32) * 0.5556)
+            };
+            break;
+    }
+}
